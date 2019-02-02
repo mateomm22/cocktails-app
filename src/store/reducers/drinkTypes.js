9ca92bf65d33fc7805069f70c57fa6b91a9df7f0
Mateo <mateo.mateusm@gmail.com>
2019-02-02 14:01:09 -0500

    Creación del store con datos quemados

---
+++ src/store/reducers/drinkTypes.js
@@ -0,0 +1,54 @@
+const initialState = [
+  {
+    "id": "1",
+    "name": "Ordinary Drink"
+  },
+  {
+    "id": "2",
+    "name": "Cocktail"
+  },
+  {
+    "id": "3",
+    "name": "Milk / Float / Shake"
+  },
+  {
+    "id": "4",
+    "name": "Other/Unknown"
+  },
+  {
+    "id": "5",
+    "name": "Cocoa"
+  },
+  {
+    "id": "6",
+    "name": "Shot"
+  },
+  {
+    "id": "7",
+    "name": "Coffee / Tea"
+  },
+  {
+    "id": "8",
+    "name": "Homemade Liqueur"
+  },
+  {
+    "id": "9",
+    "name": "Punch / Party Drink"
+  },
+  {
+    "id": "10",
+    "name": "Beer"
+  },
+  {
+    "id": "11",
+    "name": "Soft Drink / Soda"
+  }
+];
+
+
+const drinkTypesReducer = (state = initialState, action) => {
+  console.log(state);
+  return state;
+};
+
+export default drinkTypesReducer;
