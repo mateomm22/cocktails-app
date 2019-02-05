25acf5f0e5e493a70bdc0c22d1f6e6fc92856401
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-05 16:26:59 -0500

    Connect the app with the store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducers/reducer';
 
 import App from './App';
 
 import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render( app, document.getElementById( 'root' ) );
