304989e16d209aeee3ce74c3e67b7955f9222176
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-06 11:23:09 -0500

    Create the CatItem component to render each of items inside the catGroup

--- src/components/category/catGroup.js
+++ src/components/category/catGroup.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import CatItem from './catItem';
 
 
 const catGroup = (props) => {
+  let items = props.items.map(item => {
+    return (
+      <CatItem 
+        key={item.id}
+        name={item.name} />
+    )
+  });
   return (
     <div className="cat-group">
       <h4>{props.catName}</h4>
+      <div className="grid grid-4">
+        {items}
+      </div>
     </div>
   );
 }
---
+++ src/components/category/catItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const catItem = (props) => {
+  return (
+    <div className="cat-item">
+      <h3>{props.name}</h3>
+    </div>
+  );
+}
+export default catItem;
