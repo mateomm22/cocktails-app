360197fc7ddcbde53ca993aa24edb8545f765390
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-06 11:12:08 -0500

    create the catGroup component

---
+++ src/components/category/catGroup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const catGroup = (props) => {
+  return (
+    <div className="cat-group">
+      <h4>{props.catName}</h4>
+    </div>
+  );
+}
+export default catGroup;
--- src/containers/categories.js
+++ src/containers/categories.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Layout from '../misc/layout';
+import CatGroup from '../components/category/catGroup';
 
 class Categories extends Component {
   render() {
+    let catGroups = this.props.categories.map((category, idCat) => {
+      let group, items;
+      group = Object.keys(category);
+      Object.values(category).map(itemCat => items = itemCat);
+      return (
+        <CatGroup 
+          key={idCat}
+          catName={group}
+          items={items} />
+      );
+    });
+
     return(
       <Layout className="categories">
         <h1>Categories</h1>
+        {catGroups}
       </Layout>
     );
   };
