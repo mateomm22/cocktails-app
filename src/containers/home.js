38804a3e1d83a3041549cc762e3c3ae3e8e4b86c
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-07 15:50:28 -0500

    Modify the home and change the way stylus is compiled

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+import Layout from '../misc/layout';
 
 class Home extends Component {
   render() {
     return(
-      <div className="home">
+      <Layout className="home">
         <h1>Home</h1>
-      </div>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <div className="searcher">
+          <input 
+            placeholder="Search by name or ingredient" />
+          <button className="btn-send">
+            Search
+          </button>
+        </div>
+        <Link to="/categories" className="link">List categories</Link>
+      </Layout>
     );
   };
 }
