de664cbb6d7facb51410fc7cb49a4c81edf951ef
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-08 17:29:11 -0500

    create the component to render every result

--- src/containers/results.js
+++ src/containers/results.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Layout from '../misc/layout';
+import ResultItem from '../components/results/resultItem';
 
 class Results extends Component {
   render() {
-    let drinks = this.props.results.map(drink => {
-      return drink.name;
+    let drinks = this.props.results.map((drink, key) => {
+      return (
+        <ResultItem
+          key={key}
+          drinkId={drink.id} 
+          name={drink.name}
+          image={drink.image}
+          alcohol={drink.alcohol}
+          category={drink.category} />
+      );
     });
 
     return(
-      <Layout className="categories">
-        <h1>Results</h1>
-        {drinks}
+      <Layout className="results">
+        <span className="title">Results</span>
+        <div className="grid grid-4">
+          {drinks}
+        </div>
       </Layout>
     );
   };
