958d141e9821018ce9cda3826bb2ca6dfd746942
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-11 17:11:07 -0500

    Create the same workflow to fetch the alcohol level

--- src/containers/categories.js
+++ src/containers/categories.js
@@ -4,21 +4,52 @@ import { connect } from 'react-redux';
 import * as actions from '../store/actions/index';
 
 import Layout from '../misc/layout';
+import CatItem from "../components/category/catItem";
 
 class Categories extends Component {
 
   
   componentDidMount() {
     this.props.getTypes();
+    this.props.getAlc();
   }
   
   render() {
     //Watch this on the Devtools: 
-    console.log(this.props.types);
+    // console.log(this.props.alcohol);
+
+    let allTypes = this.props.types.map(type => {
+      return (
+        <CatItem 
+          key={type.id}
+          name={type.name} />
+      )
+    });
+
+    let allAlc = this.props.alcohol.map(type => {
+      if(!type.name) return;
+      return (
+        <CatItem 
+          key={type.id}
+          name={type.name} />
+      )
+    });
 
     return(
       <Layout className="categories">
         <h1>Categories</h1>
+        <div className="cat-group">
+          <h4>Drink types</h4>
+          <div className="grid grid-4">
+            {allTypes}
+          </div>
+        </div>
+        <div className="cat-group">
+          <h4>Alcohol level</h4>
+          <div className="grid grid-4">
+            {allAlc}
+          </div>
+        </div>
       </Layout>
     );
   };
@@ -27,13 +58,15 @@ class Categories extends Component {
 //Map REDUX state to LOCAL props
 const mapStateToProps  = state => {
   return {
-    types: state.drinkTypes
+    types: state.drinkTypes,
+    alcohol: state.alcohol
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    getTypes: () => dispatch(actions.fetchDrinkTypes())
+    getTypes: () => dispatch(actions.fetchDrinkTypes()),
+    getAlc: () => dispatch(actions.fetchAlcLevel()),
   };
 };
 
