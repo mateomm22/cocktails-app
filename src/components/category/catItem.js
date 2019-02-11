615159fdd0b67015e50aa9704d67f58993eb489b
Mateo Mateus <mateo.mateusm@gmail.com>
2019-02-11 17:11:07 -0500

    Create the same workflow to fetch the alcohol level

--- src/components/category/catItem.js
+++ src/components/category/catItem.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+
+
 const catItem = (props) => {
   return (
-    <div className="cat-item">
+    <div 
+      className="cat-item"
+      onClick={props.clicked}>
       <h3>{props.name}</h3>
     </div>
   );
--- src/store/reducers/alcohol.js
+++ src/store/reducers/alcohol.js
@@ -1,5 +1,30 @@
-import { initialState } from '../data/alcohol';
+import * as actions from "../actions/actionTypes";
 
-const alcoholReducer = (state = initialState, action) => state;
+const setLevels = (state, action) => {
+  
+  let results = [];
+  action.drinks.map((type, key) => {
+    let id = key;
+    let name = type.strAlcoholic;
+    let typeInfo = {
+      id: id,
+      name: name
+    };
+    return results.push(typeInfo);
+  });
+  return state = [
+    ...results
+  ]
+};
+
+const alcoholReducer = (state = [], action) => {
+  switch (action.type) {
+    case actions.GET_ALC_LEVEL:
+      return setLevels(state, action);
+  
+    default:
+      return state
+  }
+};
 
 export default alcoholReducer;
